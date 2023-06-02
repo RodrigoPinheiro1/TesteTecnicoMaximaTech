c732416bb1922abe48a9394969301e19493c8317
rodrigo pinheiro <rodrigopsps2015@gmail.com>
2023-06-02 12:15:53 -0300

    colocado dependencias e configurações de model mapper e application.properties

---
+++ src/main/java/com/testetecnicomaximatech/Config/ConfigModelMapper.java
@@ -0,0 +1,15 @@
+package com.testetecnicomaximatech.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigModelMapper {
+
+
+    @Bean
+    public ModelMapper modelMapper () {
+        return new ModelMapper();
+    }
+}
